f19b9ff12ba63247fd07d803e9997fcf1bd64883
Ankita Singh <ankitasingh9918@gmail.com>
2025-10-04 23:57:53 +0530

    Initial commit: React Counter App

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-   base: '/React_pro_two/' 
-=======
->>>>>>> df975fa (Initial commit: React Counter App)
 })
