409d18aab5cf35cef12d97c8d5a7f1ab66187c2c
Ankita Singh <ankitasingh9918@gmail.com>
2025-10-04 23:23:21 +0530

    Initial commit: React Counter App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +34,67 @@ function App() {
 }
 
 export default App
+=======
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const min = 0;
+  const max = 100;
+
+  return (
+    <div 
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ 
+        height: "100vh",              // full viewport height
+        backgroundColor: "rgb(1, 54, 98)", // dark blue background
+        fontFamily:"-moz-initial"
+      }}
+    >
+      {/* Heading */}
+      <h1 className="text-white mb-3 text-center display-4 fw-bold">Welcome to My Counter App</h1>
+
+      {/* Paragraph */}
+      <p className="text-white mb-5 text-center fs-5" style={{ maxWidth: "500px" }}>
+        This simple React Counter App lets you increase, decrease, or reset the number. 
+        You can also set limits for minimum and maximum values.
+      </p>
+
+      {/* Counter Card */}
+      <div className="card shadow p-4 text-center bg-white" style={{ width: "30rem", borderRadius: "15px" }}>
+        <div className="card-body">
+          <h3 className="card-title mb-3 display-5">Counter App</h3>
+          <h2 className="mb-4">{count}</h2>
+          <div className="d-flex justify-content-center gap-2">
+            <button
+              className="btn btn-success"
+              onClick={() => setCount(count + 1)}
+              disabled={count >= max}
+            >
+              Increase
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => setCount(count - 1)}
+              disabled={count <= min}
+            >
+              Decrease
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => setCount(0)}
+            >
+              Reset
+            </button>
+          </div>
+          <p className="mt-3 text-muted">Min: {min}, Max: {max}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> df975fa (Initial commit: React Counter App)
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
    base: '/React_pro_two/' 
+=======
+>>>>>>> df975fa (Initial commit: React Counter App)
 })
